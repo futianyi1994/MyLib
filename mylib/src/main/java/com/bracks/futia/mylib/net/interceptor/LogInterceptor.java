95fcf9bb4a506b9359039881e8f66e3285a6c813
futia <futianyi1994@126.com>
2019-05-14 17:45:09 +0800

    1.2.8：紧急修复okhttp日志拦截器在不导入okhttp3-logging-interceptor包时崩溃问题

--- mylib/src/main/java/com/bracks/futia/mylib/net/interceptor/LogInterceptor.java
+++ mylib/src/main/java/com/bracks/futia/mylib/net/interceptor/LogInterceptor.java
@@ -1,6 +1,7 @@
 package com.bracks.futia.mylib.net.interceptor;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.blankj.utilcode.util.AppUtils;
 import com.blankj.utilcode.util.LogUtils;
@@ -13,7 +14,6 @@ import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.logging.HttpLoggingInterceptor;
 import okio.Buffer;
 
 /**
@@ -25,18 +25,14 @@ import okio.Buffer;
  * @description :
  */
 public class LogInterceptor implements Interceptor {
-    private HttpLoggingInterceptor.Level level;
+    private int priority;
 
     public LogInterceptor() {
-        if (AppUtils.isAppDebug()) {
-            this.level = HttpLoggingInterceptor.Level.BODY;
-        } else {
-            this.level = HttpLoggingInterceptor.Level.NONE;
-        }
+        this.priority = AppUtils.isAppDebug() ? Log.INFO : 0;
     }
 
-    public LogInterceptor(HttpLoggingInterceptor.Level level) {
-        this.level = level;
+    public LogInterceptor(int priority) {
+        this.priority = priority;
     }
 
     @NonNull
@@ -44,7 +40,7 @@ public class LogInterceptor implements Interceptor {
     public Response intercept(@NonNull Chain chain) throws IOException {
 
         Request request = chain.request();
-        if (this.level == HttpLoggingInterceptor.Level.NONE) {
+        if (this.priority == 0) {
             return chain.proceed(request);
         }
         Request copy = request.newBuilder().build();
@@ -53,13 +49,14 @@ public class LogInterceptor implements Interceptor {
         if (body != null) {
             body.writeTo(buffer);
         }
-        LogUtils.iTag("oklog", String.format(
-                Locale.getDefault(),
-                "Request: %s Method: @%s%n%sBody: %s",
-                request.url(),
-                request.method(),
-                request.headers(),
-                buffer.readUtf8())
+        print(
+                String.format(
+                        Locale.getDefault(),
+                        "Request: %s Method: @%s%n%sBody: %s",
+                        request.url(),
+                        request.method(),
+                        request.headers(),
+                        buffer.readUtf8())
         );
 
         long startNs = System.nanoTime();
@@ -72,17 +69,43 @@ public class LogInterceptor implements Interceptor {
         }
         long tookMs = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs);
         String content = response.body().string();
-        LogUtils.iTag("oklog", String.format(
-                Locale.getDefault()
-                , "Response: %s in %dms%n%sResult：%s"
-                , response.request().url()
-                , tookMs
-                , response.headers()
-                , content)
+        print(
+                String.format(
+                        Locale.getDefault()
+                        , "Response: %s in %dms%n%sResult：%s"
+                        , response.request().url()
+                        , tookMs
+                        , response.headers()
+                        , content)
         );
         return response
                 .newBuilder()
                 .body(okhttp3.ResponseBody.create(response.body().contentType(), content))
                 .build();
+    }
+
+    private void print(String contents) {
+        switch (this.priority) {
+            case Log.VERBOSE:
+                LogUtils.vTag("oklog", contents);
+                break;
+            case Log.DEBUG:
+                LogUtils.dTag("oklog", contents);
+                break;
+            case Log.INFO:
+                LogUtils.iTag("oklog", contents);
+                break;
+            case Log.WARN:
+                LogUtils.wTag("oklog", contents);
+                break;
+            case Log.ERROR:
+                LogUtils.eTag("oklog", contents);
+                break;
+            case Log.ASSERT:
+                LogUtils.aTag("oklog", contents);
+                break;
+            default:
+                break;
+        }
     }
 }
